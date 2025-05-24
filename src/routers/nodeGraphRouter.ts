3eceaa415e67599b0e78946de0c779cebf06531a
Victor Chen <victor.chen@hoc-trade.com>
2025-05-24 17:17:43 +0800

    feature: implement nodes refresh

--- src/routers/nodeGraphRouter.ts
+++ src/routers/nodeGraphRouter.ts
@@ -1,9 +1,7 @@
 import express from "express";
-import { create } from "../controllers/node-graph-controller";
-import asyncHandler from "express-async-handler";
 
 const router = express.Router();
 
-router.post("/create", asyncHandler(create));
+// router.post("/create", asyncHandler(create));
 
 export default router;
