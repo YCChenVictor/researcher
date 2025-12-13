4c5f7d279418318142a5093e5ea2e809334f636d
Victor Chen <victor.chen@hoc-trade.com>
2025-12-13 20:48:33 +0800

    refactor: remove-backend-dir

--- backend/src/schemas/helloWorld.ts
+++
@@ -1,7 +0,0 @@
-import { z } from "../swagger/zod";
-
-const helloWorldResponseSchema = z.object({
-  message: z.string(),
-});
-
-export { helloWorldResponseSchema };
--- backend/src/swagger/zod.ts
+++
@@ -1,6 +0,0 @@
-import { z as baseZod } from "zod";
-import { extendZodWithOpenApi } from "@asteasolutions/zod-to-openapi";
-
-extendZodWithOpenApi(baseZod);
-
-export const z = baseZod;
